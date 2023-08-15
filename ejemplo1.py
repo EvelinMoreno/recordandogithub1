b96c46c2723be8e5a8941f30eb7a8536d4ba88c1
alexgz23 <aleton2312@gmail.com>
2023-08-15 08:06:17 -0500

    feat:creamos variables primitivas y lista de datos

---
+++ ejemplo1.py
@@ -0,0 +1,13 @@
+#comentario linea 
+'''comentario'''
+
+nombreUsuario="Evelin Yulieth Moreno Parra"
+edadUsusario=20
+estaturaUsuario=1.50
+esHinchaDelVerde=True
+
+print(f"su nombre es: {nombreUsuario} y tiene {edadUsusario} años.")
+
+comidasFavoritas=["frijoles","pescado","papas","carne frita"]
+print(comidasFavoritas)
+print(comidasFavoritas[2])
